c5fa62bc600c5f558ba8c31b5dcd489c84337ad1
J2a0s0o0n <jb-2000@gmx.de>
2017-05-25 19:22:59 +0200

    Combination of Toast and Wood with Oven now possible

--- Source/Hut.py
+++ Source/Hut.py
@@ -1,5 +1,5 @@
 """
-the Hut
+test
 """
 import EngineL.Core
 
@@ -34,8 +34,61 @@ class Jam(EngineL.Core.Entity):
         EngineL.Core.Entity.__init__(self, parent)
         self.setObjectName(EngineL.Core.get_res_man().get_string("game.places.hut.jam.name"))
 
+class Oven(EngineL.Core.StaticEntity):
+    """
+    the oven
+    """
+
+    def __init__(self, parent=None):
+        EngineL.Core.StaticEntity.__init__(self, parent)
+        self.set_state("on", 0)
+    def on_used(self, user, other_entity=None):
+        """
+        This non-constant, overriden method does something.
+        """
+        if isinstance(other_entity, Wood):
+            if self.get_state("on") == 0:
+                self.set_state("on", 1)
+                other_entity.setParent(None)
+                return True
+            else:
+                return False
+        elif isinstance(other_entity, Toast):
+            if other_entity.get_state("toasted") == 0 and self.get_state("on") == 1:
+                other_entity.set_state("toasted", 1)
+                other_entity.setParent(None)
+                return True
+            else:
+                return False
+        else:
+            return False
+
+class Wood(EngineL.Core.Entity):
+    """
+    the wood
+    """
+    def __init__(self, parent=None):
+        EngineL.Core.Entity.__init__(self, parent)
+        self.setObjectName(EngineL.Core.get_res_man().get_string("game.places.yard.wood.name"))
+    def on_used(self, user, other_entity=None):
+        if isinstance(other_entity, Oven):
+            return other_entity.on_used(user, self)
+
+
+class Toast(EngineL.Core.Entity):
+    """
+    the Toast
+    """
+    def __init__(self, parent=None):
+        EngineL.Core.Entity.__init__(self, parent)
+        self.set_state("toasted", 0)
+        self.setObjectName(EngineL.Core.get_res_man().get_string("game.places.hut.toast.name"))
+    def on_used(self, user, other_entity=None):
+        if isinstance(other_entity, Oven):
+            return other_entity.on_used(user, self)
+
 def register_entity_classes(app):
     """
     This function registers all of our new Entity classes to the given application instance.
     """
-    app.register_entity_classes([Sofa, Jam])
+    app.register_entity_classes([Sofa, Jam, Oven, Toast, Wood])
